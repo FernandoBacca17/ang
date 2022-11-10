acd8c64fd7cda10c50438edde3c991f23e8e6cd4
FernandoBacca17 <56320879+FernandoBacca17@users.noreply.github.com>
2022-11-10 02:04:14 -0500

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RestService } from './rest.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang';
+  public listdevacunas:Array<any> = []
+
+  constructor(private RestService:RestService){
+
+  }
+
+  ngOnInit(): void{
+    this.cargarData();
+  }
+
+  public cargarData(){
+    this.RestService.get('https://evening-badlands-77809.herokuapp.com/api/vacunas')
+    .subscribe(respuesta => {
+      console.log(respuesta);
+    })
+  }
 }
